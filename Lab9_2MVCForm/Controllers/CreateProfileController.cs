33516337db76690a2256b8121d489866a9de9e3e
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-09-09 16:44:00 -0400

    Completed first two exercises

---
+++ Lab9_2MVCForm/Controllers/CreateProfileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab9_2MVCForm.Controllers
+{
+    public class CreateProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateProfile()
+        {
+            return View();
+        }
+
+        public IActionResult MyPage(string firstName, string lastName)
+        {
+            ViewData["FirstName"] = firstName;
+            ViewData["LastName"] = lastName;
+
+            return View();
+        }
+    }
+}
---
+++ Lab9_2MVCForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab9_2MVCForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
